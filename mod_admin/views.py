f34fcac2829adc6438c1a972c18722f30b64a7c9
peyman_safiri <peyman_safiri@yahoo.com>
2022-08-21 19:08:45 +0430

    Update User authentication

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,4 +1,6 @@
-from flask import session
+from flask import session , render_template , request , abort
+from mod_users.forms import LoginForm
+from mod_users.models import User
 from . import admin 
 
 @admin.route('/')
@@ -6,9 +8,24 @@ def index():
 	return "Hello from Admin"
 
 
-@admin.route('/login/')
+@admin.route('/login/', methods=['GET' , 'POST'])
 def login():
-	session['name']= 'Peyman'
-	print(session.get('name'))
-	print(session)
-	return "Hello from login"
+	form = LoginForm(request.form)
+	if request.method == 'POST':
+		if not form.validate_on_submit():
+			abort(400)
+	    # user = User.query.filter( User.email == form.email.data ).first()
+		user = User.query.filter(User.email.ilike(f'form.email.data')).first()
+		if not user:
+			return "Incorect Credentials" , 400 
+		if not user.check_password(form.password.data):
+		    return "incorrect password "
+		session['email'] = user.email
+		session['id'] = user.id
+		return "You Logged in successfully"	
+	if session.get('email') is not None:
+		return "You are Already Logged in"		
+	return render_template('admin/login.html',form=form) 
+    	
+
+	
